e9f854d946a955180a947c5eb504921d38bdcb58
ormxmi <36085168+ormxmi@users.noreply.github.com>
2018-02-06 19:54:51 +0200

    mobile improvements

--- main.js
+++ main.js
@@ -1,6 +1,64 @@
 $(document).ready(function(){
-    var markTextFn,underScoreFn;var spanIdLeft = 1 ;var blackRectSpanList = document.getElementsByClassName("blackRectSpan");
-    var heightPage = $(document).height();var underline = 1;
+    var markTextFn,markTextFnMobile,underScoreFn,underScoreFnInterval;var spanIdLeft = 0 ;var blackRectSpanList = document.getElementsByClassName("blackRectSpan");
+    var heightPage = $(document).height();var underline = 1;var isMobile = false;
+    markTextFn= setInterval(function(){
+                
+        if(spanIdLeft==0){$('.blackRectSpan').css("background-color","rgba(216, 86, 88,0)")
+        }
+        
+        $(blackRectSpanList[spanIdLeft-1]).css("mix-blend-mode","color");
+        $(blackRectSpanList[spanIdLeft]).css("mix-blend-mode","lighten");
+        $(blackRectSpanList[spanIdLeft]).css("background-color","rgba(216, 86, 88,0.7)");
+        $(blackRectSpanList[spanIdLeft-1]).css("background-color","rgba(216, 86, 88,0)");
+       
+        
+        
+        spanIdLeft++;
+        if(spanIdLeft==blackRectSpanList.length+1){spanIdLeft=0;}
+       
+   },100);
+    clearInterval(markTextFn);
+    if(window.innerWidth<=480){isMobile=true;}
+    if(isMobile){
+        markTextFnInterval=1500;
+        $('.whiteRect>h1').html('IDENTITY<span id="whiteRectUnder">_</span>');
+        markTextFnMobile = setInterval(()=>{ /*if the device is a mobile phone, have the interval markTextFn reocurring the                                           whole time ... cause hover doesnt work quite well with phones.**/
+        markTextFn= setInterval(function(){  
+                console.log(spanIdLeft);
+         if(spanIdLeft==9){clearInterval(markTextFn);
+                           $(blackRectSpanList[spanIdLeft-1]).css("background-color","rgba(216, 86, 88,0)");
+                          $(blackRectSpanList[spanIdLeft-1]).css("mix-blend-mode","color");
+                   }      
+        if(spanIdLeft==0){$('.blackRectSpan').css("background-color","rgba(216, 86, 88,0)")
+        }
+        
+        $(blackRectSpanList[spanIdLeft-1]).css("mix-blend-mode","color");
+        $(blackRectSpanList[spanIdLeft]).css("mix-blend-mode","lighten");
+        $(blackRectSpanList[spanIdLeft]).css("background-color","rgba(216, 86, 88,0.7)");
+        $(blackRectSpanList[spanIdLeft-1]).css("background-color","rgba(216, 86, 88,0)");
+       
+        
+        
+        spanIdLeft++;
+        if(spanIdLeft==blackRectSpanList.length+1){spanIdLeft=0;}
+       
+   },100)//end of markTextFn
+        },4000);//end of markTextFnMobile 
+       
+        underScoreFn = setInterval(function(){
+        if(underline%2!=0)
+        {$('#whiteRectUnder').css("color","rgba(255,255,255,0)");
+        }
+        else
+        {$('#whiteRectUnder').css("color","rgba(255,255,255,1)");
+        }
+        underline++;
+         },466)
+    } //end of if(isMobile)
+    window.onresize = function(){ 
+        if(window.innerWidth <= 480){
+        location.reload();}
+}
     $('.left').click(()=>{
         $('.left').css("width","50%");
         $('.right').css("width","50%");
@@ -25,9 +83,12 @@ $(document).ready(function(){
         $('html,body').animate({scrollTop:heightPage},777)
     })
     $('.blackRect>h1').hover(function(){
-        
         $(this).css("cursor","pointer");
-        markTextFn= setInterval(function(){
+        if(!isMobile){
+        
+       markTextFn= setInterval(function(){  
+                console.log(spanIdLeft);
+         
         if(spanIdLeft==0){$('.blackRectSpan').css("background-color","rgba(216, 86, 88,0)")
         }
         
@@ -35,24 +96,31 @@ $(document).ready(function(){
         $(blackRectSpanList[spanIdLeft]).css("mix-blend-mode","lighten");
         $(blackRectSpanList[spanIdLeft]).css("background-color","rgba(216, 86, 88,0.7)");
         $(blackRectSpanList[spanIdLeft-1]).css("background-color","rgba(216, 86, 88,0)");
-        
+       
         
         
         spanIdLeft++;
         if(spanIdLeft==blackRectSpanList.length+1){spanIdLeft=0;}
-    },100)
+       
+   },100)
+        
+    }   
     },function(){
-        clearInterval(markTextFn); $('.blackRect').find('span').css("background-color","rgba(0,0,0,0)");
+        if(!isMobile){
+        clearInterval(markTextFn);
+        clearInterval(markTextFnMobile);
+        $('.blackRect').find('span').css("background-color","rgba(0,0,0,0)");
         $('.blackRect').find('span').css("mix-blend-mode","color");
         spanIdLeft = 0;
-       
+        }
     })
     
      $('.whiteRect').hover(function(){
+         if(!isMobile){
         $('.whiteRect>h1').css("border","3px solid rgba(255,255,255,1)");
         $('.whiteRect>h1').css("cursor","pointer");
         $('#whiteRectUnder').css("display","inline");
-         $('#whiteRectUnder').css("color","rgba(255,255,255,1)");
+        $('#whiteRectUnder').css("color","rgba(255,255,255,1)");
         underScoreFn = setInterval(function(){
         if(underline%2!=0)
         {$('#whiteRectUnder').css("color","rgba(255,255,255,0)");
@@ -62,11 +130,14 @@ $(document).ready(function(){
         }
         underline++;
          },466)
+         }
     },function(){
+         if(!isMobile){
          clearInterval(underScoreFn); 
          underline = 1;
          $('#whiteRectUnder').css("color","rgba(255,255,255,0)");
-        $('.whiteRect>h1').css("border","3px solid rgba(255,255,255,0)");
+         $('.whiteRect>h1').css("border","3px solid rgba(255,255,255,0)");
+         }
     }) 
     $('#volumeOnBtn').click(()=>{
         var volume = document.getElementById("bgPlay");
@@ -101,7 +172,9 @@ $(document).ready(function(){
 var randomWords = ["tick tock", "12/03/1998","チョコレート","どこで","クロック","pursued","vending","pity","excess"];
 var randomWord = 0,lastRandomWord=0,h1tag="",lasth1tag="",word="",i=0,lasti=0;
 var randWidth = 0,lastRandWidth=0,randWidthCSS="";
+if(!isMobile){
 setInterval(function(){
+
     while(randWidth==lastRandWidth){randWidth= Math.floor((Math.random() * 80) + 10);}
     lastRandWidth=randWidth;
     randWidthCSS=randWidth.toString()+"vw";
@@ -123,6 +196,6 @@ setInterval(function(){
     
 },460);
     
-    
+}//if device width >= 480px...
     
 })
